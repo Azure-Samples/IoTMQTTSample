73b77484aa2bf853b4c622caa2f002ff384d08f0
Ryan Winter <ryanwinter@outlook.com>
2023-03-08 11:48:37 -0800

    revise device twin sample to better track request id.
    Signed-off-by: Ryan Winter <ryanwinter@outlook.com>

--- mosquitto/src/mosquitto_device_twin.cpp
+++ mosquitto/src/mosquitto_device_twin.cpp
@@ -13,49 +13,58 @@
 #define PORT     8883
 #define USERNAME HOST "/" DEVICEID "/?api-version=2020-09-30"
 
-#define DEVICEMESSAGE            "devices/" DEVICEID "/messages/#"
 #define DEVICETWIN_SUBSCRIPTION  "$iothub/twin/res/#"
-#define DEVICETWIN_MESSAGE_GET   "$iothub/twin/GET/?$rid=123"
-#define DEVICETWIN_MESSAGE_PATCH "$iothub/twin/PATCH/properties/reported/?$rid=patch_temp"
+#define DEVICETWIN_MESSAGE_GET   "$iothub/twin/GET/?$rid=%d"
+#define DEVICETWIN_MESSAGE_PATCH "$iothub/twin/PATCH/properties/reported/?$rid=%d"
+
+static unsigned int device_twin_request_id = 0;
 
 void connect_callback(struct mosquitto* mosq, void* obj, int result)
 {
-    printf("Connect callback returned result: %s\r\n", mosquitto_strerror(result));
-    if (result == MOSQ_ERR_CONN_REFUSED)
+    printf("Connect callback result: %s\r\n", mosquitto_strerror(result));
+    if (result != MOSQ_ERR_SUCCESS)
     {
-        printf("Connection refused. Please check DeviceId, IoTHub name or if your SAS Token has expired.\r\n");
+        printf("Connection error. Please check DeviceId, IoTHub name or if your SAS Token has expired.\r\n");
+        return;
     }
-    else
+
+    printf("\r\nSetting device twin reported properties....\r\n");
+
+    char msg[] = "{\"temperature\": 32}";
+    char mqtt_publish_topic[64];
+    snprintf(mqtt_publish_topic, sizeof(mqtt_publish_topic), DEVICETWIN_MESSAGE_PATCH, device_twin_request_id++);
+
+    int rc = mosquitto_publish(mosq, NULL, mqtt_publish_topic, sizeof(msg) - 1, msg, 1, true);
+    if (rc != MOSQ_ERR_SUCCESS)
     {
-        printf("\r\nSetting device twin reported properties....\r\n");
-        int msgId  = rand();
-        char msg[] = "{\"temperature\": 32}";
-        int rc     = mosquitto_publish(mosq, &msgId, DEVICETWIN_MESSAGE_PATCH, sizeof(msg) - 1, msg, 1, true);
-        if (rc != MOSQ_ERR_SUCCESS)
-        {
-            printf("Error: %s\r\n", mosquitto_strerror(rc));
-        }
+        printf("Error: %s\r\n", mosquitto_strerror(rc));
     }
 }
 
 void disconnect_callback(struct mosquitto* mosq, void* obj, int rc)
 {
-    printf("Disconnect callback %d : %s\r\n", rc, mosquitto_strerror(rc));
+    printf("Disconnect callback result: %s\r\n", mosquitto_strerror(rc));
 }
 
 void message_callback(struct mosquitto* mosq, void* obj, const struct mosquitto_message* message)
 {
     printf("Device twin message '%.*s' for topic '%s'\r\n", message->payloadlen, (char*)message->payload, message->topic);
 
-    const char getTwinTopic[]   = "$iothub/twin/res/200/?$rid=123";
-    const char patchTwinTopic[] = "$iothub/twin/res/204/?$rid=patch_temp";
+    const char patchTwinTopic[] = "$iothub/twin/res/204/?$rid=0";
+    const char getTwinTopic[]   = "$iothub/twin/res/200/?$rid=1";
+
     if (strncmp(message->topic, patchTwinTopic, sizeof(patchTwinTopic) - 1) == 0)
     {
+        // Process the reported property response and request the device twin
         printf("Setting device twin properties SUCCEEDED.\r\n\r\n");
+
         printf("Getting device twin properties....\r\n");
-        int msgId  = rand();
-        char msg[] = "";
-        int rc     = mosquitto_publish(mosq, &msgId, DEVICETWIN_MESSAGE_GET, sizeof(msg) - 1, msg, 1, true);
+
+        char msg[] = "{}";
+        char mqtt_publish_topic[64];
+        snprintf(mqtt_publish_topic, sizeof(mqtt_publish_topic), DEVICETWIN_MESSAGE_GET, device_twin_request_id++);
+
+        int rc = mosquitto_publish(mosq, NULL, mqtt_publish_topic, sizeof(msg) - 1, msg, 1, true);
         if (rc != MOSQ_ERR_SUCCESS)
         {
             printf("Error: %s\r\n", mosquitto_strerror(rc));
@@ -63,7 +72,9 @@ void message_callback(struct mosquitto* mosq, void* obj, const struct mosquitto_
     }
     else if (strncmp(message->topic, getTwinTopic, sizeof(getTwinTopic) - 1) == 0)
     {
+        // Process the device twin response and stop the client
         printf("Getting device twin properties SUCCEEDED.\r\n\r\n");
+
         mosquitto_loop_stop(mosq, false);
         mosquitto_disconnect(mosq); // finished, exit program
     }
